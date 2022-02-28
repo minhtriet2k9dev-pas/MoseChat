fd2c15ec3b4f014c92522eb0a014cf301f153718
minhtriet2k9 <minhtrietamoled@gmail.com>
2022-02-28 09:59:06 +0700

    update many things

--- backend/server/index.js
+++ backend/server/index.js
@@ -7,6 +7,8 @@ const { Server } = require("socket.io");
 
 const io = new Server(server);
 
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "..", "..", "frontend/html/index.html"));
 });
@@ -21,8 +23,8 @@ app.get("/js/index.js", (req, res) => {
 	res.sendFile(path.join(__dirname, "..", "..", "frontend/js/index.js"));
 });
 
-server.listen(3000, () => {
-	console.log(`Server is listening on port 3000`);
+server.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
 });
 
 io.on("connection", (socket) => {
